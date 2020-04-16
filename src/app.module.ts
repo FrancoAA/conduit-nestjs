45e1c4cdba3c3247d70690178292ea6a4f580469
Franco Alarcon <franco.alarcon@attackiq.com>
2020-04-16 19:30:59 -0300

    - creates users, profiles, articles & tags modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { ArticlesModule } from './articles/articles.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProfilesModule, ArticlesModule, TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
