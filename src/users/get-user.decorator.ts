4852d063f1e2107ae6247a83eee2a4305bcdcd11
Franco Alarcon <franco.alarcon@attackiq.com>
2020-04-18 17:12:17 -0300

    🐛 Upgrades get-user decorator to new API

--- src/users/get-user.decorator.ts
+++ src/users/get-user.decorator.ts
@@ -1,8 +1,9 @@
-// import { createParamDecorator } from '@nestjs/common';
-// import { User } from './user.entity';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from './schemas/user.interface';
 
-// export const GetUser = createParamDecorator(
-//   (data, req): User => {
-//     return req.user;
-//   },
-// );
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
--- src/users/schemas/user.interface.ts
+++ src/users/schemas/user.interface.ts
@@ -1,6 +1,7 @@
 import { Document } from 'mongoose';
 
 export interface User extends Document {
+  readonly _id: String;
   readonly username: String;
   readonly password: String;
   readonly email: String;
